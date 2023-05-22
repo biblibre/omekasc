381dfa50234bf213cfa49a966dd3aa947fc5e6be
Boubacar OUATTARA <boubaca.ouatara@biblibre.com>
2023-05-22 10:38:18 +0200

    cette fonction  permet d afficher les modules
    sur leurs informations(etat,version)

--- src/Command/ListModulesCommand.php
+++ src/Command/ListModulesCommand.php
@@ -5,6 +5,7 @@ namespace Omeka\Console\Command;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Formatter\OutputFormatterStyle;
 
 class ListModulesCommand extends Command
 {
@@ -18,31 +19,53 @@ class ListModulesCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+
         $omekaHelper = $this->getHelper('omeka');
         $application = $omekaHelper->getApplication();
         $services = $application->getServiceManager();
         $modules = $services->get('Omeka\ModuleManager')->getModules();
 
+        $legend = [
+            'green' => 'activé',
+            'yellow' => 'non activé / non installé',
+            'red' => 'non trouvé / module invalide / module.ini invalide / version de omeka invalide',
+            'magenta' => 'mise à niveau nécessaire',
+
+        ];
+        $output->writeln('Légende :');
+        foreach ($legend as $color => $description) {
+            $outputStyle = new OutputFormatterStyle($color);
+            $output->getFormatter()->setStyle($color, $outputStyle);
+            $output->writeln(sprintf('<%s>%s</%s>', $color, $description, $color));
+        }
+
+        $output->writeln('');
+
         foreach ($modules as $module) {
             $name = $module->getName();
             $moduleStates = [
                 'active' => ['description' => 'activé', 'color' => 'green'],
-                'not_active' =>['description' => 'non activé', 'color' => 'yellow'],
+                'not_active' => ['description' => 'non activé', 'color' => 'yellow'],
                 'not_installed' => ['description' => 'non installé', 'color' => 'yellow'],
                 'not_found' => ['description' => 'non trouvé', 'color' => 'red'],
                 'invalid_module' => ['description' => 'module invalide', 'color' => 'red'],
                 'invalid_ini' => ['description' => 'module.ini invalide', 'color' => 'red'],
                 'invalid_omeka_version' => ['description' => 'version de omeka invalide', 'color' => 'red'],
-                'needs_upgrade' => ['description' => 'mise à niveau nécessaire', 'color' => 'orange'],
+                'needs_upgrade' => ['description' => 'mise à niveau nécessaire', 'color' => 'magenta'],
             ];
             $state = $module->getState();
             $stateInfo = $moduleStates[$state];
-            $description =  $stateInfo['description'];
-            $color =  $stateInfo['color'];
+            $description = $stateInfo['description'];
+            $color = $stateInfo['color'];
             $ini = $module->getIni();
             $version = $ini['version'];
-            $output->writeln(sprintf("%s - v%s\n %s", $name, $version, $description));
+
+            $outputStyle = new OutputFormatterStyle($color);
+            $output->getFormatter()->setStyle($state, $outputStyle);
+
+            $output->writeln(sprintf('<%s>%s - version(%s)</%s>', $state, $name, $version, $state));
         }
+
         return Command::SUCCESS;
     }
 }
