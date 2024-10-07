23b6ff4436cc954884a38e524c71f411206f18d5
Julian Maurice <julian.maurice@biblibre.com>
2024-10-07 15:07:26 +0200

    Rewrite the "set password" command
    * Change its name to user:set-password to match the style of other
      command names
    * Email and password are now arguments instead of options
    * Do not ask the password twice
    * Ask password in `interact` method instead of `execute`

--- src/Command/UserSetPasswordCommand.php
+++ src/Command/UserSetPasswordCommand.php
@@ -3,87 +3,72 @@
 namespace Omeka\Console\Command;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
 class UserSetPasswordCommand extends Command
 {
-    protected static $defaultName = 'SetPassword';
+    protected static $defaultName = 'user:set-password';
 
     protected function configure()
     {
         $this->setDescription('Set a new password');
-        $this->addOption('email', '', InputOption::VALUE_REQUIRED);
-        $this->addOption('newPassword', '', InputOption::VALUE_REQUIRED);
-        $this->addOption('confirmPassword', '', InputOption::VALUE_REQUIRED);
+        $this->addArgument('email', InputArgument::REQUIRED);
+        $this->addArgument('password', InputArgument::REQUIRED);
     }
 
     protected function interact(InputInterface $input, OutputInterface $output)
     {
         $io = new SymfonyStyle($input, $output);
-        $io = new SymfonyStyle($input, $output);
 
-        $userEmail = $input->getOption('email');
-        if(null === $userEmail) {
-            $userEmail = $io->ask('Enter the user\'s email', $userEmail, function ($userEmail) {
-                return ($userEmail);
-            });
-            $input->setOption('email', $userEmail);
+        while (null === $input->getArgument('email')) {
+            $email = $io->ask("Enter the user's email");
+            $input->setArgument('email', $email);
         }
 
+        while (null === $input->getArgument('password')) {
+            $password = $io->askHidden("Enter the new password");
+            $input->setArgument('password', $password);
+        }
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $io = new SymfonyStyle($input, $output);
+
+        $email = $input->getArgument('email');
+        if (null === $email) {
+            $io->error('Missing argument: email');
+            return Command::FAILURE;
+        }
+
+        $password = $input->getArgument('password');
+        if (null === $password) {
+            $io->error('Missing argument: password');
+            return Command::FAILURE;
+        }
+
         $omekaHelper = $this->getHelper('omeka');
         $application = $omekaHelper->getApplication();
         $services = $application->getServiceManager();
-        $entityManager = $services->get('Omeka\EntityManager');
-        $userRepository = $entityManager->getRepository('Omeka\Entity\User');
-
-        $io = new SymfonyStyle($input, $output);
+        $em = $services->get('Omeka\EntityManager');
 
         try {
-            $userTarget = $userRepository->findOneBy(['email' => $input->getOption('email')]);
+            $user = $em->getRepository('Omeka\Entity\User')->findOneBy(['email' => $email]);
+            if ($user === null) {
+                throw new \Exception('User does not exist: ' . $email);
+            }
+
+            $user->setPassword($password);
+            $em->flush();
+            $io->success("Password has been changed");
         } catch(\Exception $e) {
             $io->error($e->getMessage());
             return Command::FAILURE;
         }
 
-        if (empty($userTarget)) {
-            $io->error('User does not exist ' . $userEmail);
-            return Command::FAILURE;
-        }
-
-        if($userTarget) {
-
-            $newPassword = $input->getOption('newPassword');
-            if(null === $newPassword) {
-                $newPassword = $io->askHidden('Enter the new password', $newPassword, function ($newPassword) {
-                    return ($newPassword);
-                });
-
-                $input->setOption('newPassword', $newPassword);
-                $confirmPassword = $io->askHidden('Confirm the password', null, function ($confirmPassword) {
-                    return ($confirmPassword);
-                });
-
-            }
-
-            if ($newPassword === $confirmPassword) {
-
-                $userTarget->setPassword($newPassword);
-                $entityManager->persist($userTarget);
-                $entityManager->flush();
-                $io->success("Password has been changed");
-                return Command::SUCCESS;
-            }
-
-            $io->error("Passwords must be the same");
-            return Command::FAILURE;
-        }
-
+        return Command::SUCCESS;
     }
 }
